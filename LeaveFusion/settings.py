1b47a96a22c44fcf97f2f3bb810123589ccdbbf1
pranjulps3 <pranjul.ps3@gmail.com>
2017-08-28 20:06:32 +0530

    UI tweeks

--- LeaveFusion/settings.py
+++ LeaveFusion/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v0o)kux-3d_nigwc=bnq!josh)0ctcgxvjoao^jbquc8nulbg2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['172.27.73.21']
 
