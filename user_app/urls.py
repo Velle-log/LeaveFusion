8c02b3a47edc03d56343057203c14b0bce0f3fc6
pranjulps3 <pranjul.ps3@gmail.com>
2017-08-29 19:35:48 +0530

    Authentication Constraints Updated

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from user_app import views
 
 urlpatterns = [
-    url(r'^$', views.profile_view, name='profile_view'),
+    url(r'^(?P<id>[0-9]+)/', views.profile_view, name='profile_view'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,28 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from  django.http import Http404
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+def handler404(request):
+  response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+  response.status_code = 404
+  return response
+
+
+def handler500(request):
+  response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+  response.status_code = 500
+  return response
+
+
 @login_required(login_url='/accounts/login/')
 def index(request):
 	return render(request, 'index/home.html', {'title':'Home'})
 
 
 @login_required(login_url='/accounts/login/')
-def profile_view(request):
-	return render(request, 'index/profile.html', {'title':request.user.username})
-	
-
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
-def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
+def profile_view(request, id):
+  try:
+    user = User.objects.get(id = id)
+  except:
+    return Http404('Page not found :/')                               #TODO: raise an http404 exception so to reach the 404 page
+  return render(request, 'user_app/profile.html', {'title':user.username, 'user': user})
