35034528ff74888a04b72e9c48c8063b608d7252
Saket Patel <saket.is.sam@gmail.com>
2017-09-15 01:59:28 +0000

    production change

--- LeaveFusion/apache/wsgi.py
+++ LeaveFusion/apache/wsgi.py
@@ -7,7 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+apache_configuration= os.path.dirname(__file__)
+project = os.path.dirname(apache_configuration)
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+sys.path.append(project)
+
+
+import logging
+logging.basicConfig(stream=sys.stderr)
 
 from django.core.wsgi import get_wsgi_application
 
